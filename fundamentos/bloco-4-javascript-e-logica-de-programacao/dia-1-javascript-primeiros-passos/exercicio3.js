59e12de1bd3a806bde8d7e16138b0fcfe205850e
Guilherme Lara <guilaraa@gmail.com>
2021-11-26 19:13:37 -0300

    Adiciona o exercicio 3

---
+++ fundamentos/bloco-4-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+const x = 3;
+const y = 4;
+const z = 5;
+
+if(x > y && x > z){
+    console.log('"x" é maior que "y" e "z"!');
+} else if(y > x && y > z){
+    console.log('"y" é maior que "x" e "z"!');
+} else{
+    console.log('"z" é maior que "x" e "y"!');
+}
