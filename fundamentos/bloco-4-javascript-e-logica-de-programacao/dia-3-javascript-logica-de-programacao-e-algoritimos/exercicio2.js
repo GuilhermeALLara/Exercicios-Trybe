2c76d44b38d20dda9b5304056c8357aa0063ccc1
Guilherme Lara <guilaraa@gmail.com>
2021-11-30 19:57:11 -0300

    adiciona exercicios do dia

---
+++ fundamentos/bloco-4-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio2.js
@@ -0,0 +1,10 @@
+let n = 5;
+let simbolo = '*';
+let line = '';
+
+//No caso abaixo o for vai vai verificar se a linha atende a condição, tamanho do n, atendendo essa condição ele desenha a linha e adiciona um contador até atingir o valor de N.
+for(index = 0; index <= n; index += 1){
+    console.log(line);
+    line = line + simbolo;
+}
+
